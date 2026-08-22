7aa05bc3003d272df30d37ccd8c940cff2a9f2ca
DiegoMenegaldo <diego.menegaldo@sptech.school>
2026-08-22 19:15:36 -0300

    Separação entre backend e frontend

---
+++ backend/src/main/java/com/alittlepieceofmysoulreimagined/gothic/GothicApplication.java
@@ -0,0 +1,13 @@
+package com.alittlepieceofmysoulreimagined.gothic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GothicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GothicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alittlepieceofmysoulreimagined/gothic/GothicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alittlepieceofmysoulreimagined.gothic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GothicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
