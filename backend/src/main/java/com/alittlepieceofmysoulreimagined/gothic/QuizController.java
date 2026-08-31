fca76151246bd7951e166360d18fe06b59daecef
DiegoMenegaldo <diego.menegaldo@sptech.school>
2026-08-30 21:55:09 -0300

    Inserindo perguntas do Quiz, falta da tabela associativa

--- backend/src/main/java/com/alittlepieceofmysoulreimagined/gothic/QuizController.java
+++ backend/src/main/java/com/alittlepieceofmysoulreimagined/gothic/QuizController.java
@@ -1,10 +1,13 @@
 package com.alittlepieceofmysoulreimagined.gothic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.PreparedStatement;
 
 @CrossOrigin
 @RestController
@@ -12,4 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    @PostMapping("/inserirPerguntas")
+    public ResponseEntity<Quiz> inserirPerguntas(@RequestBody Quiz quiz){
+        if (quiz == null) {
+            return ResponseEntity.status(400).build();
+        }
+
+        if (quiz == null ||
+            quiz.getP1() == null || quiz.getP1().isBlank() ||
+            quiz.getP2() == null || quiz.getP2().isBlank() ||
+            quiz.getP3() == null || quiz.getP3().isBlank() ||
+            quiz.getP4() == null || quiz.getP4().isBlank() ||
+            quiz.getP5() == null || quiz.getP5().isBlank() ||
+            quiz.getP6() == null || quiz.getP6().isBlank() ||
+            quiz.getP7() == null || quiz.getP7().isBlank()) {
+            return ResponseEntity.status(400).build();
+        }
+
+        String sql = "INSERT INTO Quiz (p1, p2, p3, p4, p5, p6, p7) VALUES (?,?,?,?,?,?,?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection ->{
+            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, quiz.getP1());
+            ps.setString(2, quiz.getP2());
+            ps.setString(3, quiz.getP3());
+            ps.setString(4, quiz.getP4());
+            ps.setString(5, quiz.getP5());
+            ps.setString(6, quiz.getP6());
+            ps.setString(7, quiz.getP7());
+            return ps;
+        },keyHolder);
+        Integer idQuiz = keyHolder.getKeyAs(Integer.class);
+        quiz.setIdQuiz(idQuiz);
+        return ResponseEntity.status(201).body(quiz);
+    }
 }
--- backend/target/classes/schema.sql
+++ backend/target/classes/schema.sql
@@ -21,49 +21,56 @@ CREATE TABLE Cadastro(
 );
 
 CREATE TABLE Quiz(
-                     idQuiz INT PRIMARY KEY AUTO_INCREMENT,
-                     p1 CHAR(1) NOT NULL,
-                     p2 CHAR(1) NOT NULL,
-                     p3 CHAR(1) NOT NULL,
-                     p4 CHAR(1) NOT NULL,
-                     p5 CHAR(1) NOT NULL,
-                     p6 CHAR(1) NOT NULL,
-                     p7 CHAR(1) NOT NULL
+    idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+    p1 CHAR(1) NOT NULL,
+    p2 CHAR(1) NOT NULL,
+    p3 CHAR(1) NOT NULL,
+    p4 CHAR(1) NOT NULL,
+    p5 CHAR(1) NOT NULL,
+    p6 CHAR(1) NOT NULL,
+    p7 CHAR(1) NOT NULL
 );
 
 
 CREATE TABLE ResultadosQuiz(
-                               idResultado INT AUTO_INCREMENT,
-                               fkCadastro INT,
-                               fkQuiz INT,
-                               resultado varchar(45) NOT NULL,
-                               dtResultado TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                               PRIMARY KEY(idResultado, fkCadastro, fkQuiz),
-                               FOREIGN KEY (fkCadastro) REFERENCES Cadastro(idCadastro),
-                               FOREIGN KEY (fkQuiz) REFERENCES Quiz(idQuiz)
+    idResultado INT AUTO_INCREMENT,
+    fkCadastro INT,
+    fkQuiz INT,
+    resultado varchar(45) NOT NULL,
+    dtResultado TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(idResultado, fkCadastro, fkQuiz),
+    FOREIGN KEY (fkCadastro) REFERENCES Cadastro(idCadastro),
+    FOREIGN KEY (fkQuiz) REFERENCES Quiz(idQuiz)
 );
 
 INSERT INTO Banda (nome) VALUES
-                             ('Bauhaus'),
-                             ('The Cure'),
-                             ('Siouxsie and the Banshees'),
-                             ('The Sisters of Mercy'),
-                             ('Christian Death'),
-                             ('Lebanon Hanover'),
-                             ('She Past Away'),
-                             ('Pink Turns Blue'),
-                             ('Twin Tribes'),
-                             ('Ultra Sunn'),
-                             ('London After Midnight'),
-                             ('She Wants Revenge'),
-                             ('His Infernal Majesty (HIM)');
+    ('Bauhaus'),
+    ('The Cure'),
+    ('Siouxsie and the Banshees'),
+    ('The Sisters of Mercy'),
+    ('Christian Death'),
+    ('Lebanon Hanover'),
+    ('She Past Away'),
+    ('Pink Turns Blue'),
+    ('Twin Tribes'),
+    ('Ultra Sunn'),
+    ('London After Midnight'),
+    ('She Wants Revenge'),
+    ('His Infernal Majesty (HIM)');
 
 INSERT INTO Genero (nome) VALUES
-                              ('Gothic Rock'),
-                              ('Post-Punk'),
-                              ('Darkwave'),
-                              ('Coldwave'),
-                              ('Industrial'),
-                              ('Deathrock'),
-                              ('EBM (Electronic Body Music)'),
-                              ('Gothic Metal / Love Metal');
+    ('Gothic Rock'),
+    ('Post-Punk'),
+    ('Darkwave'),
+    ('Coldwave'),
+    ('Industrial'),
+    ('Deathrock'),
+    ('EBM (Electronic Body Music)'),
+    ('Gothic Metal / Love Metal');
+
+INSERT INTO Cadastro (nomeCompleto, email, dtNascimento, senha, fkBanda, fkGenero) VALUES
+    ('Larissa Iceglass', 'larissa.iceglass@lebanonhanover.com', '1988-10-18', 'Darkwave#2026!', 6, 3),
+    ('Peter Murphy', 'peter.murphy@bauhaus.com', '1957-07-11', 'GothRock@1979!', 1, 1),
+    ('Ville Valo', 'ville.valo@him.com', '1976-11-22', 'LoveMetal$666*', 13, 8),
+    ('Andrew Eldritch', 'andrew.eldritch@sistersofmercy.com', '1959-05-15', 'TempleOfLove#87', 4, 1),
+    ('Patricia Morrison', 'patricia.morrison@sisters.com', '1962-01-14', 'Lucretia@1987!', 4, 6);
